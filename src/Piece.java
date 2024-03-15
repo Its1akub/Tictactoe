d6c8be7be72dcf5d3d48fbca5e5781be787bbe83
Its_1akub <jacobspernoga@gmail.com>
2024-03-15 12:38:31 +0100

    add Piece

---
+++ src/Piece.java
@@ -0,0 +1,11 @@
+public class Piece {
+    private char piece;
+    private int x;
+    private int y;
+
+    public Piece(char piece, int x, int y) {
+        this.piece = piece;
+        this.x = x;
+        this.y = y;
+    }
+}
