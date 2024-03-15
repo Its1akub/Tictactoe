520c124926b0ba3cf23c1a0f3c966c5ed0de1f2b
Its_1akub <jacobspernoga@gmail.com>
2024-03-15 13:16:18 +0100

    add method playerWin - check if player 1 win (return 1) or player 2 win (return 2) if draw return 0

--- src/Mechanics.java
+++ src/Mechanics.java
@@ -1,29 +1,45 @@
 import java.util.Arrays;
 
 public class Mechanics {
+    private int win;
     private char[][] board = new char[3][3];
 
-    public boolean playerWin(char[][] board, Player player) {
-        if(board[0][0] == player.getSymbol()&&board[1][1] ==player.getSymbol()&&board[2][2] ==player.getSymbol()){
-            return true;
-        } else if (board[0][0] == player.getSymbol()&&board[0][1] ==player.getSymbol()&&board[0][2] ==player.getSymbol()){
-            return true;
-        } else if (board[1][0] == player.getSymbol()&&board[1][1] == player.getSymbol()&&board[1][2] ==player.getSymbol()){
-            return true;
-        } else if (board[2][0] == player.getSymbol()&&board[2][1] ==player.getSymbol()&&board[2][2] ==player.getSymbol()){
-            return true;
-        } else if (board[0][0] == player.getSymbol()&&board[1][0] ==player.getSymbol()&&board[2][0] ==player.getSymbol()){
-            return true;
-        } else if (board[0][1] == player.getSymbol()&&board[1][1] ==player.getSymbol()&&board[2][1] ==player.getSymbol()){
-            return true;
-        } else if (board[0][2] == player.getSymbol()&&board[1][2] == player.getSymbol()&&board[2][2] ==player.getSymbol()){
-            return true;
-        } else if (board[2][0] == player.getSymbol()&&board[1][1] ==player.getSymbol()&&board[0][2] ==player.getSymbol()){
-            return true;
-        }else {
-            return false;
+    public int playerWin(char[][] board, Player p1, Player p2) {
+        if(board[0][0] == p1.getSymbol()&&board[1][1] ==p1.getSymbol()&&board[2][2] ==p1.getSymbol()){
+            return 1;
+        } else if (board[0][0] == p1.getSymbol()&&board[0][1] ==p1.getSymbol()&&board[0][2] ==p1.getSymbol()){
+            return 1;
+        } else if (board[1][0] == p1.getSymbol()&&board[1][1] == p1.getSymbol()&&board[1][2] ==p1.getSymbol()){
+            return 1;
+        } else if (board[2][0] == p1.getSymbol()&&board[2][1] ==p1.getSymbol()&&board[2][2] ==p1.getSymbol()){
+            return 1;
+        } else if (board[0][0] == p1.getSymbol()&&board[1][0] ==p1.getSymbol()&&board[2][0] ==p1.getSymbol()){
+            return 1;
+        } else if (board[0][1] == p1.getSymbol()&&board[1][1] ==p1.getSymbol()&&board[2][1] ==p1.getSymbol()){
+            return 1;
+        } else if (board[0][2] == p1.getSymbol()&&board[1][2] == p1.getSymbol()&&board[2][2] ==p1.getSymbol()){
+            return 1;
+        } else if (board[2][0] == p1.getSymbol()&&board[1][1] ==p1.getSymbol()&&board[0][2] ==p1.getSymbol()){
+            return 1;
+        } else if(board[0][0] == p2.getSymbol()&&board[1][1] ==p2.getSymbol()&&board[2][2] ==p2.getSymbol()){
+            return 2;
+        } else if (board[0][0] == p2.getSymbol()&&board[0][1] ==p2.getSymbol()&&board[0][2] ==p2.getSymbol()){
+            return 2;
+        } else if (board[1][0] == p2.getSymbol()&&board[1][1] == p2.getSymbol()&&board[1][2] ==p2.getSymbol()){
+            return 2;
+        } else if (board[2][0] == p2.getSymbol()&&board[2][1] ==p2.getSymbol()&&board[2][2] ==p2.getSymbol()){
+            return 2;
+        } else if (board[0][0] == p2.getSymbol()&&board[1][0] ==p2.getSymbol()&&board[2][0] ==p2.getSymbol()){
+            return 2;
+        } else if (board[0][1] == p2.getSymbol()&&board[1][1] ==p2.getSymbol()&&board[2][1] ==p2.getSymbol()){
+            return 2;
+        } else if (board[0][2] == p2.getSymbol()&&board[1][2] == p2.getSymbol()&&board[2][2] ==p2.getSymbol()){
+            return 2;
+        } else if (board[2][0] == p2.getSymbol()&&board[1][1] ==p2.getSymbol()&&board[0][2] ==p2.getSymbol()){
+            return 2;
+        } else {
+            return 0;
         }
-
     }
 
 
