073b7a6f7d99d1a8f0e59a368523950642dc9e38
Its_1akub <jacobspernoga@gmail.com>
2024-03-15 13:26:27 +0100

    add method playerWin - check if player 1 win (return 1) or player 2 win (return 2) if draw return 0

--- src/Mechanics.java
+++ src/Mechanics.java
@@ -3,8 +3,16 @@ import java.util.Arrays;
 public class Mechanics {
     private int win;
     private char[][] board = new char[3][3];
+    private Plaier p1;
+    private Plaier p2;
 
-    public int playerWin(char[][] board, Player p1, Player p2) {
+    public Mechanics(char[][] board, Plaier p1, Plaier p2) {
+        this.board = board;
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    public int playerWin(char[][] board, Plaier p1, Plaier p2) {
         if(board[0][0] == p1.getSymbol()&&board[1][1] ==p1.getSymbol()&&board[2][2] ==p1.getSymbol()){
             return 1;
         } else if (board[0][0] == p1.getSymbol()&&board[0][1] ==p1.getSymbol()&&board[0][2] ==p1.getSymbol()){
@@ -37,9 +45,10 @@ public class Mechanics {
             return 2;
         } else if (board[2][0] == p2.getSymbol()&&board[1][1] ==p2.getSymbol()&&board[0][2] ==p2.getSymbol()){
             return 2;
-        } else {
+        } else if (!Arrays.asList(board).contains(' ')){
             return 0;
         }
+        return Integer.parseInt(null);
     }
 
 
